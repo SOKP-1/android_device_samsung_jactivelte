b0a84e52e9c648e578a7ceccdde37066af9a8789
Arun Manohar Manoharan <email2arunmanohar@gmail.com>
2014-06-27 00:32:42 +0530

    SOKPify

--- sokp.mk
+++ sokp.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/samsung/jactiveltexx/full_jactiveltexx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/sokp/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SOKP stuff.
+$(call inherit-product, vendor/sokp/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jactiveltexx TARGET_DEVICE=jactiveltexx BUILD_FINGERPRINT="samsung/jactiveltexx/jactivelte:4.2.2/JDQ39/I9295XXUAMF7:user/release-keys"
 
 PRODUCT_DEVICE := jactiveltexx
-PRODUCT_NAME := cm_jactiveltexx
+PRODUCT_NAME := sokp_jactiveltexx
 
